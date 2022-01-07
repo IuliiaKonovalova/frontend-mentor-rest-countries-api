2e16e4b40ccb164110096fcf638ec45b99116777
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-01-07 22:01:23 +0800

    Add all data to the html

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,76 +20,57 @@ document.addEventListener('DOMContentLoaded', function () {
 
 });
 
-async function requestAllData() {
-  //Request Data from local json file on the page load 
-  const response = await fetch('countries.json');
-  const data = await response.json();
 
-  dataContainer.forEach(element => {
-    for (let i = 0; i < data.length; i++) {
-      dataContainer[i].textContent = data[i].name[0];
-      //timeCurrent[i].textContent = data[i].timeframes.daily.current + (data[i].timeframes.daily.current > 1 ? "hrs" : "hr");
-      //timePrevious[i].textContent = "Yesterday - " + data[i].timeframes.daily.previous + (data[i].timeframes.daily.previous > 1 ? "hrs" : "hr");
-    }
-  });
-}
-requestAllData();
-fetch("https://restcountries.com/v3.1/all")
+
+fetch("assets/js/countries.json")
   .then(res => res.json())
   .then(data => initialize(data))
+  // .then(data => console.log(data))
   .catch(err => console.log("Error:", err));
+
+// fetch("../services/contributors.JSON")
+// .then(res => res.json())
+// .then(data => console.log(data))
 
 function initialize(countriesData) {
   countries = countriesData;
   console.log(countries[32])
   // code
-  console.log(`code ${countries[50].altSpellings[0]}`)
-  // native name
-  // console.log(`native name ${countries[0].altSpellings[1]}`)
-  // native name
-  // console.log(`native name ${countries[0].altSpellings[2]}`)
-  // native name
-  // console.log(`OFFICIAL name ${countries[0].altSpellings[3]}`)
+  console.log(`code ${countries[32].alpha3Code}`)
+
   //common name
-  console.log(`COMMON NAME        ${countries[50].name.common}`)
+  console.log(`COMMON NAME        ${countries[50].name}`)
   // official name
-  console.log(`OFFICIAL NAME          ${countries[50].name.official}`)
+  // console.log(`OFFICIAL NAME          ${countries[50].name.official}`)
   // native name
-  console.log(`NATIVE NAME        ${countries[50].name.nativeName.eng.common}`)
+  console.log(`NATIVE NAME        ${countries[50].nativeName}`)
 
   // borders
-  if (!("borders" in countries[32])) {
-    console.log("there is no borders")
-  } else {
-    console.log(countries[32].borders[0]) //could be more
-    for (borderingCountry of countries[32].borders) {
-      console.log(borderingCountry)
-    }
-  }
+  console.log(countries[32].borders) //could be more
+
   //capital
-  console.log(countries[50].capital[0])
+  console.log(countries[32].capital)
   //population
-  console.log(countries[50].population)
+  console.log(countries[32].population)
   //region
   console.log(`region    ${countries[50].region}`)
   //domain
-  for (domain of countries[50].tld) {
-    console.log(domain)
-  }
-  console.log(countries[50].tld[0])
-  //subregion
-  console.log(` subregions       ${countries[50].subregion}`)
-  console.log(`continents ${countries[50].continents[0]}`)
-  // for (let currenc of countries[50].currencies) {
-  //   console.log(currenc)
-
+  // for (domain of countries[50].tld) {
+  //   console.log(domain)
   // }
-  console.log(countries[50].currencies)
-  console.log(countries[50].flags.png)
+  console.log(countries[50].topLevelDomain[0])
+  //subregion
+  console.log(` subregions       ${countries[50].subregion}`) /
+    // for (let currenc of countries[50].currencies) {
+    //   console.log(currenc)
 
-  console.log(countries[50].languages) //could be more
+    // }
+    console.log(countries[50].currencies[0].name)
+  console.log(countries[50].flag)
 
-  console.log(countries[50].maps.googleMaps)
+  console.log(countries[50].languages[0].name) //could be more
+
+  // console.log(countries[50].maps.googleMaps)
   let options = "";
 
   // console.log(countries[0].name)
@@ -109,6 +90,16 @@ function initialize(countriesData) {
 
 function displayCountryData(countryByCode) {
   const countryData = countries.find(country => country.altSpellings[0] === countryByCode);
-  countryCapital.innerHTML = countryData.capital[0];
+  countryName.innerHTML = countryData.name;
+  countryNativeName.innerHTML = countryData.nativeName;
+  countryPopulation.innerHTML = countryData.population.toLocaleString("en-US")
+  countryRegion.innerHTML = countryData.region;
+  countrySubregion.innerHTML = countryData.subregion;
+  countryCapital.innerHTML = countryData.capital;
+  countryDomain.innerHTML = countryData.topLevelDomain[0]
+  countryLanguages.innerHTML = countryData.languages[0].name
 
+
+
+  countryCurrencies.innerHTML = countryData.currencies[0].name;
 }
